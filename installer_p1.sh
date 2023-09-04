595305c18753418ce68c6ff0df5f762f45a58d00
VGFletcher <125897513+VGFletcher@users.noreply.github.com>
2023-09-04 14:03:21 +0100

    Split Installer to add flag

---
+++ installer_p1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Load Modules
+module purge
+
+module load GCCcore/10.2.0
+module load Python/3.8.6
+module load GCC/10.2.0
+module load OpenMPI/4.0.5
+module load OpenMPI/4.0.5
+module load ASE/3.21.1
+
+#Download lammps
+git clone -b release https://github.com/lammps/lammps.git
+
+#Install Additional Packages
+cd lammps/src/
+
+make yes-EXTRA-COMPUTE
+make yes-EXTRA-FIX
+make yes-MANYBODY
+make yes-MEAM
+make yes-PYTHON
