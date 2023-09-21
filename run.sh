0270ff6a60bfc5df9427d69603418f7ffa1d6a3b
VGFletcher <125897513+VGFletcher@users.noreply.github.com>
2023-09-21 17:20:33 +0100

    Corrected run due to modules

--- run.sh
+++ run.sh
@@ -9,15 +9,5 @@ module load GCC/11.3.0
 module load OpenMPI/4.1.4
 module load ASE/3.22.1
 
-#Compile lammps
-make -j 40 serial mode=shlib
-
-#Create python package
-make install-python
-
-#Create link to python package
-cd ../../
-ln -s lammps/src/liblammps_serial.so ./liblammps_serial.so
-
-#Install pymatnest
-git clone https://github.com/libAtoms/pymatnest.git
+#Run pymatnest you may want/need to change the number of threads I've put 40
+mpirun -n 40 ./pymatnest/ns_run <mg_stand.inp>> mg_stand.out
